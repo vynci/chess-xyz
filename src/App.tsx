cc23973efc8e2fbb714d9b2778c7183ae63a95e3
nbelizaga <nbelizaga@gmail.com>
2025-07-20 09:34:48 -0400

    added dec button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(10)
 
   return (
     <>
@@ -16,14 +16,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{count}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount((count) => count*2)}>
+Increment        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count - 1)}>
+Decrement        </button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
